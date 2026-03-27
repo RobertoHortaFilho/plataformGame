20993c573be925ed7e1e82076ba6a2f6e1d38a20
RobertoFilho <robertohortafilho@gmail.com>
2026-03-27 00:09:35 -0300

    fix: pause screen not blink
    fix no erro onde a tela de pause piscava na tela durante a gameplay

--- scripts/pause_game.gd
+++ scripts/pause_game.gd
@@ -1,27 +1,24 @@
 extends CanvasLayer
 
-func _ready() -> void:
-	$Control/Container/Continue.pressed.connect(resume)
-	$Control/Container/MainMenu.pressed.connect(main_menu)
-	#$Control.visible = false
-	$Control/AnimationPlayer.stop()
-	
-func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("esc"
-	) and get_tree().paused == false
-	) and !get_tree().current_scene.scene_file_path == "res://scene/inicial_screen.tscn":
-		get_tree().paused = true
-		#$Control.visible = true
-		$Control/AnimationPlayer.play("open")
-		
+@onready var ani: AnimationPlayer = $AnimationPlayer
 
-func resume():
-	$Control/AnimationPlayer.play_backwards("open")
-	await $Control/AnimationPlayer.animation_finished
-	get_tree().paused = false
-	#$Control.visible = false
+
+func _ready():
+	$Screen/MidleScreen/CenterContainer/VBoxContainer/Resume.pressed.connect(_on_resume)
+	$Screen/MidleScreen/CenterContainer/VBoxContainer/Menu.pressed.connect(_on_mainmenu)
 	
-func main_menu():
+	
+func _process(_delta: float) -> void:
+	if (Input.is_action_pressed("esc") and get_tree().paused == false
+	) and get_tree().current_scene.name != "InicialScreen":
+		get_tree().paused = true
+		ani.play("Open")
+
+func _on_resume():
 	get_tree().paused = false
-	$Control.visible = false
+	ani.play("Close")
+	
+func _on_mainmenu():
+	get_tree().paused = false
+	ani.play("Close")
 	SceneTransition.change_transition("res://scene/inicial_screen.tscn")
